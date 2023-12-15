6479902703452545ff22ba9e052b2b44639e2d6e
wadenya <alexwadenya001@gmail.com>
2023-12-15 02:34:42 -0800

    0x02. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,23 +3,28 @@
 Minimum Operations
 """
 
+import math
+
+
+def factors(n):
+    """factors of n"""
+    mylist = []
+    while n % 2 == 0:
+        mylist.append(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            mylist.append(i)
+            n = n / i
+    if n > 2:
+        mylist.append(n)
+    return mylist
+
+
 def minOperations(n):
-    if n <= 0:
+    """calculate the min operations"""
+    if type(n) != int or n < 2:
         return 0
-
-    # Init an array to store the minimum operations needed for each numb
-    dp = [float('inf')] * (n + 1)
-
-    # Base case: 0 operations needed to reach 1 H
-    dp[1] = 0
-
-    # Iterate from 2 to n and fill in the dp array
-    for i in range(2, n + 1):
-        # Check if i is divisible by any j (2 <= j <= i)
-        for j in range(2, i + 1):
-            if i % j == 0:
-                # Copy All and Paste j times
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    # Return the minimum operations needed for n
-    return dp[n] if dp[n] != float('inf') else 0
+    else:
+        numOperations = sum(factors(n))
+        return int(numOperations)
